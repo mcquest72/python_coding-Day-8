b664dbe73a9c314b4a9916368e81fa69c4136076
McQuest-72 <mcquest2347@gmail.com>
2024-05-03 13:47:11 -0800

    A function called greet was defined and 3 block of print statement was added to it

---
+++ greet.py
@@ -0,0 +1,17 @@
+
+# This program contains a function which enables greetings.
+
+# Review
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+print()
+
+def greet():
+    print('Hello World!')
+    print('Welcome to CyberArmy,')
+    print('To be one of us, you must master our technologies.')
+
+greet()
+print()
